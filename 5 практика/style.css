466374d6e8d50c09d979ad9782062199a036d546
SashaPolJack <106552046+SashaPolJack@users.noreply.github.com>
2022-12-22 23:47:15 +0300

    Add files via upload

---
+++ 5 практика/style.css
@@ -0,0 +1,339 @@
+@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
+@media(max-width:760px){
+    .menu>li{
+        font-family: 'Lato', sans-serif;
+    color: black;
+    font-size: 2em;
+    background-color: #4e5680;
+    width: 100%;
+    text-align: center;
+    padding-top: 1em;
+    border: 1px solid;
+    font-weight: bold;
+    height: 65px;
+    }
+    .div_bur,.div_bur::after,.div_bur::before{
+        width: 3em;
+    position: absolute;
+    background-color: black;
+    height: 0.5em;
+        transition-duration: 200ms;
+    border-radius: 4px;
+       right: 0;
+    
+    margin-top: 1px;
+        
+        
+    }
+    .div_bur{
+       margin-right: 32px; 
+    }
+    #ch{
+        display: none;
+    }
+    .nav_m{
+        display: none;
+    }
+    #ch:checked ~ .nav_m{
+        display: block;
+    }
+    #ch:checked + .ch_l>.div_bur{
+        transform: rotate(45deg);
+        
+    }
+    
+    #ch:checked + .ch_l>.div_bur::after{
+transform: rotate(90deg);
+         margin-top: 0px;
+    }
+    #ch:checked + .ch_l>.div_bur::before{
+     transform: rotate(90deg);
+         margin-top: 0px;
+        
+    }
+    .div_bur::after{
+        content: '';
+        margin-top: 13px;   
+    }
+     .div_bur::before{
+        content: '';
+        margin-top: -13px;   
+    }
+    .grop_pic{
+        margin-top: 0px !important; 
+    }
+    .im_g{
+        margin-top: 50px;
+        width: 100% !important;
+    }
+    .nav_m{
+            padding-top: 99px;
+    width: 100%;
+    height: 49px;
+        margin-left: 0px;
+    }
+    .menu{
+        list-style-type: none;
+    display: flex;
+    padding-left: 0px;
+    flex-wrap: wrap;
+    width: 100%;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
+    flex-direction: column;
+    }
+    .top_h{
+        background-color: #131e56bf;
+    position: fixed;
+    height: 5em;
+    width: 100%;
+    
+    align-items: center;
+    }
+    
+    .logo{
+        display: none;
+    }
+    
+}
+.div_gar{
+   
+}
+.im_g{
+    width: 300px;
+    height: 300px;
+}
+.grop_pic{
+    margin-top: 50px;
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: space-around;
+}
+.div_it_pic{
+    
+    height: 300px;
+    background-color: black;
+    
+    
+}
+.el_menu{
+       
+    
+   
+}
+a{
+     text-decoration: none;
+
+    transition-duration: 1s;
+}
+body{
+    margin: 0;
+}
+.sty{
+    position: sticky;
+    top: 20px;
+}
+.rel_pos_footer{
+        position: relative;
+    left: 184px;
+    top: 16px;
+}
+.top_h{
+      background-color: #131e56bf;
+    position: fixed;
+    height: 5em;
+    width: 100%;
+    display: flex;
+    align-items: center;
+
+}
+#Head{
+    position: sticky;
+}
+main{
+    
+       padding-top: 58px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
+
+}
+.fl_b{
+        display: flex;
+    flex-direction: row;
+    width: 50%;
+    justify-content: space-around;
+}
+.menu{
+    list-style-type: none;
+    display: flex;
+    width: 100%;
+    justify-content: space-around;
+}
+nav{
+        padding-top: 1px;
+    width: 100%;
+    height: 49px;
+        margin-left: 18px;
+}
+.plit > li + li{
+    color: aqua;
+    
+}
+a:visited{
+    color: chartreuse;
+}
+a:active{
+    color: black;
+}
+a:hover{
+    color: blueviolet;
+    font-size: 20px;
+    
+}
+.logo, .menu{
+    position: fixed;
+}
+
+.image{
+    width: 200px;
+    height: 200px;
+    font-weight: bold;
+}
+.o_stat{
+    
+    float: left;
+}
+.logo{
+    width: 5%;
+}
+h1,h2,h3,lable{
+    font-family: 'Lato', sans-serif;
+}
+.menu>li{
+    font-family: 'Lato', sans-serif;
+    color: black;
+    font-size: 2em;
+    font-weight: bold;
+}
+li:nth-child(3){
+    color: brown;
+}
+.podval{
+        border: solid 1px blue;
+
+}
+.top_butt{
+    background-color: aquamarine;
+    width: 89px;
+    border-radius: 20px;
+    height: 68px;
+    padding-top: 2px;
+        position: fixed;
+    right: 3%;
+    bottom: 3%;
+}
+form{
+    display: flex;
+    flex-direction: column;
+}
+.pole_t{
+    margin-top: 2%;
+    margin-bottom: 6%;
+    width: 100%;
+    border-radius: 8px;
+    height: 3em;
+    background-color: #4e568066;
+    border-width: 0px;
+    padding-left: 3%;
+    transition-duration: 500ms;
+}
+.pole_t:focus{
+      background-color: #70a2fd99;
+}
+
+.but_tex{
+    font-family: 'Lato', sans-serif;
+    text-align: center;
+    font-size: 24px;
+    margin: 0;
+    margin-top: 16px;
+}
+.float_b{
+        width: 57%;
+}
+.file_i{
+    margin-top: 3%;
+    margin-bottom: 7%;
+}
+.check_box{
+        margin-bottom: 6%;
+}
+fieldset{
+    border-radius: 17px;
+    border-color: aqua;
+    margin-bottom: 7%;
+}
+.date_v{
+    margin-top: 2%;
+    margin-bottom: 10%;
+}
+.submit_f{
+    width: 100%;
+    height: 38px;
+    border-radius: 11px;
+    background-color: #67c0e3;
+    transition-duration: 1s;
+}
+.submit_f:hover{
+    background-color: #028bc1;
+}
+iframe{
+        width: 50%;
+    height: 385px;
+}
+textarea::placeholder{
+    font-family: 'Lato', sans-serif;
+    color: #325f87;
+    padding: 5px;
+    font-size: 14px;
+}
+.lists{
+    
+}
+.div_a{
+       background-color: red;
+    border-radius: 2%;
+    display: flex;
+    justify-content: center;
+}
+.text_A{
+   text-align: center;
+    font-size: 5em;
+        font-family: 'Lato', sans-serif;
+}
+.group_div_B_E{
+  display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    height: 238px;
+    justify-content: center;
+}
+.div_b{
+   
+    background-color: aqua;
+}
+.div_e{
+
+    background-color: green;
+}
+.text_b{
+        font-size: 2em;
+}
+.text_e{
+        font-size: 2em;
+}
